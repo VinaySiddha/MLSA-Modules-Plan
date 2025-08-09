f5633eb4a78b817cf43d9ca764a4f5d3925f3a0a
VinaySiddha <22a81a4359@sves.org.in>
2025-08-09 06:35:25 +0530

    vinay

---
+++ demo.py
@@ -0,0 +1,17 @@
+
+import os
+import random
+
+random.random()
+os.access("demo.py", os.R_OK)
+# print("Hello, World!")
+
+
+
+x = int(input("Enter a number: "))
+y = int(input("Enter another number: "))
+sum = x + y
+
+
+
+print("The sum is:", sum)
